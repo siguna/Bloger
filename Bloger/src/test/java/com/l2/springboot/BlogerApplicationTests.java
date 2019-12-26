793e79529d0cd3aa60acddf89887c80fc5c5dc7e
locdo98 <44898288+locdo98@users.noreply.github.com>
2019-12-27 00:51:05 +0700

    test

---
+++ Bloger/src/test/java/com/l2/springboot/BlogerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
